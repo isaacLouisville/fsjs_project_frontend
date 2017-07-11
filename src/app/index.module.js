3aa7756d2d792891ed570ae8bc1dfab5d55e200b
isaac <calliper.jennings@gmail.com>
2017-07-11 18:18:24 -0400

    Alan Blount troubleshot this commit

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -5,7 +5,7 @@ import { routerConfig } from './index.route';
 import { runBlock } from './index.run';
 import { MainController } from './main/main.controller';
 import { AuthController } from './auth/auth.controller';
-import { CompareToDirective} from './directive/CompareTo.directive';
+//import { CompareToDirective} from './directive/CompareTo.directive';
 import { GithubContributorService } from '../app/components/githubContributor/githubContributor.service';
 import { WebDevTecService } from '../app/components/webDevTec/webDevTec.service';
 import { NavbarDirective } from '../app/components/navbar/navbar.directive';
@@ -23,4 +23,5 @@ angular.module('myMessageFront', ['ui.router', 'ui.bootstrap', 'toastr'])
   .controller('AuthController', AuthController)
   .directive('acmeNavbar', NavbarDirective)
   .directive('acmeMalarkey', MalarkeyDirective)
-  .directive('compareTo', CompareToDirective);
+  //.directive('compareTo', CompareToDirective)
+;
